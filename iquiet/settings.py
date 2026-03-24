cbcbb830cbb3ded130c132fe98d478ab90da665c
ITSROHITTECH <srivastavarohitkumar067@gmail.com>
2026-03-24 19:04:06 +0530

    .

--- iquiet/settings.py
+++ iquiet/settings.py
@@ -47,9 +47,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -138,9 +138,12 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
+CORS_ALLOW_HEADERS = ['*']
+CORS_ALLOW_METHODS = ['*']
 
 CORS_ALLOW_CREDENTIALS = True
+
+
 
 CORS_ALLOWED_ORIGINS = [
     "https://iquiet.vercel.app",
